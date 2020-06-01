a6e3e980e79acbffa0fd8daa451ed2ec66b35577
Sajaki <Sajaki9@gmail.com>
2020-06-01 20:08:20 +0200

    fix

--- testdb.php
+++ testdb.php
@@ -1,28 +1,18 @@
 <?php
-# Fill our vars and run on cli
-# $ php -f db-connect-test.php
-
+# test database
 $dbname = 'phpbb';
 $dbuser = 'phpbb';
 $dbpass = 'geheim2';
 $dbhost = 'container1';
 
-
 $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($link, $test_query);
+$sql = "SELECT COUNT(*) as tablecount FROM information_schema.tables WHERE table_schema = '$dbname' ";
+$result = mysqli_query($link, $sql);
+$value = mysqli_fetch_array($result);
+ 
+$tblCnt = is_array($value) ? $value[0] : 0;
+echo "There are $tblCnt phpBB tables<br />\n";
 
-$tblCnt = 0;
-while($tbl = mysqli_fetch_array($result)) {
-  $tblCnt++;
-  #echo $tbl[0]."<br />\n";
-}
-
-if (!$tblCnt) {
-  echo "There are no tables<br />\n";
-} else {
-  echo "There are $tblCnt phpBB tables<br />\n";
-} 
 ?>
